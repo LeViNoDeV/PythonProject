cf753e7b999f6a47b46e2eba0621e01f0ce6aa10
Adam Lange <101813311+Adamthedevoloper@users.noreply.github.com>
2022-05-25 13:39:32 +0300

    Update gameprorace.py

--- racegameadam/gameprorace.py
+++ racegameadam/gameprorace.py
@@ -1,9 +1,11 @@
 
+
 from tkinter import*
 import time
 import random
 import sys
 import os
+
 
 
 
@@ -26,6 +28,110 @@ horse2_y = 120
 horse3_x = 0
 horse3_y = 220
 
+#main screen
+main_screen =Tk()
+main_screen.title('horse race')
+main_screen.geometry('800x600')
+main_screen.config(background='WHITE')
+
+#horse labels
+one =Label(main_screen, text = '1',font=('calibri',25),fg='red',bg='white' )
+one.place(x=10,y =50)
+two =Label(main_screen, text = '2',font=('calibri',25),fg='red',bg='white' )
+two.place(x=10,y =150)
+three =Label(main_screen, text = '3',font=('calibri',25),fg='red',bg='white' )
+three.place(x=10,y =250)
+
+#creating entery box 
+def button_command():
+    global helpc
+    
+    text = entery1.get()
+    if text =='1':
+        helpc = 1
+        l5 =Label(main_screen, text = 'your horse is: horse 1',font=('calibri',20),fg='red',bg='white' )
+        l5.place(x=10,y =460)
+        
+        l1.place_forget()
+        entery1.pack_forget()
+        b3.pack_forget()
+        
+        entery2.pack()
+        b4.pack()
+        l6 =Label(main_screen, text = 'place your bet',font=('calibri',20),fg='red',bg='white' )
+        l6.place(x=330,y =380)
+    if text =='2':
+        helpc = 2
+        l5 =Label(main_screen, text = 'your horse is: horse 2',font=('calibri',20),fg='red',bg='white' )
+        l5.place(x=10,y =460)
+        entery1.delete(0,899) 
+        l1.place_forget()   
+        entery1.pack_forget()
+        b3.pack_forget()
+        entery2.pack()
+        b4.pack()
+        l6 =Label(main_screen, text = 'place your bet',font=('calibri',20),fg='red',bg='white' )
+        l6.place(x=330,y =380)
+
+    if text =='3':
+        helpc = 3
+        l5 =Label(main_screen, text = 'your horse is: horse 3',font=('calibri',20),fg='red',bg='white' )
+        l5.place(x=10,y =460)
+        
+        l1.place_forget()  
+        entery1.pack_forget()
+        b3.pack_forget()
+        entery2.pack()
+        b4.pack()
+        l6 =Label(main_screen, text = 'place your bet',font=('calibri',20),fg='red',bg='white' )
+        l6.place(x=330,y =380)
+    return None
+
+
+
+
+
+#casino chips
+
+entery2 = Entry(main_screen , width=20 )
+entery2.pack_forget()
+textbet = entery2.get()
+
+casinochips =10000
+chiptxt=Label(main_screen, text = 'chips: ' ,font=('calibri',20),fg='red',bg='white')
+chiptxt.place(x=10 ,y=500)
+lchip = Label(main_screen, text = casinochips ,font=('calibri',20),fg='red',bg='white')
+lchip.place(x=80,y=500)
+
+
+
+def button_command2():
+    textbet = entery2.get()
+    texthelp = int(textbet)
+    
+    
+    if texthelp>0 and texthelp<=10000:
+        lchip.place_forget()
+        chips = casinochips-texthelp
+        lchip.place_forget
+        chip2 = Label(main_screen, text =  chips ,font=('calibri',20),fg='red',bg='white')
+        chip2.place(x=80,y=500)
+        entery2.pack_forget()
+        betlab =Label(main_screen, text = 'your bet is:   '+ textbet,font=('calibri',20),fg='red',bg='white' )
+        betlab.place(x=330,y =380)
+        b1.place(x= 280,y=490)
+        b4.pack_forget()
+        
+        betlab.place_forget
+    
+        
+        
+   
+    
+
+
+
+
 
 
 def start_game():
@@ -34,31 +140,43 @@ def start_game():
     global horse1_x
     global horse3_x
     global winner
-    global casinochips
+    import chips
+    textbet = entery2.get()
+    texthep = int(textbet)
     
-    
-    
+    chip = casinochips-texthep
+    Num= texthep*2
+    wincips = chip+Num
+    losecips = chip - texthep
     
     while winner ==False:
         time.sleep(0.03)
         random_move_horse_2 = random.randrange(0,20)
-        random_move_horse_1 = random.randrange(0,20)
+        random_move_horse_1 = random.randrange(00,20)
         random_move_horse_3 = random.randrange(0,20)
         #update x pos from both horses
         horse2_x += random_move_horse_2
         horse1_x += random_move_horse_1
-        horse3_x +=random_move_horse_3
+        horse3_x += random_move_horse_3
 
         move_horses(random_move_horse_1,random_move_horse_2,random_move_horse_3)
         main_screen.update()
         winner = chek_winner()
+   
     if winner =="TIE":
-        Label(main_screen,text=winner,font=('calibri',40),fg= 'red',bg ='white').place(x= 200,y= 290)
+        
+        
+        Label(main_screen,text=winner,font=('calibri',40),fg= 'red',bg ='white').place(x= 350, y= 425)
+        horsewin.place_forget()
+       
+       
     if winner == 'your horse has won':
-        Label(main_screen,text=winner,font=('calibri',40),fg = 'red',bg ='white').place(x= 250, y= 350)
-        Label(main_screen,text=  casinochips*2,font=('calibri',20),fg = 'red',bg ='white').place(x= 100, y= 500)
-    else:    Label(main_screen,text=winner + ' wins',font=('calibri',40),fg = 'red',bg ='white').place(x= 250, y= 350)
-
+        Label(main_screen,text=winner,font=('calibri',40),fg = 'red',bg ='white').place(x= 220, y= 350)
+        Label(main_screen,text=wincips ,font=('calibri',20),fg = 'red',bg ='white').place(x= 80, y= 500)
+    else:
+            horsewin = Label(main_screen,text=winner + ' wins',font=('calibri',40),fg = 'red',bg ='white')
+            horsewin.place(x= 230, y= 350)
+            Label(main_screen,text=losecips ,font=('calibri',20),fg = 'red',bg ='white').place(x= 80, y= 500)
 
 
 
@@ -71,7 +189,7 @@ def chek_winner():
     
     global helpc
     
-    if horse2_x >= 550 and horse1_x >=550 and horse3_x >510:
+    if horse2_x >= 550 and horse1_x >=550 and horse3_x >550:
         return "TIE"
     if horse2_x >= 550 and horse1_x >=550:
         return 'TIE'    
@@ -100,8 +218,7 @@ def chek_winner():
 
 
 
-def End_game():
-    exit()
+
 
 
 
@@ -113,22 +230,17 @@ def new_round():
     os.execl(python, python, * sys.argv)
     
 #buttons
-#main screen
-main_screen =Tk()
-main_screen.title('horse race')
-main_screen.geometry('800x600')
-main_screen.config(background='white')
 
 
 
 #canvas setup
-Canvas =Canvas(main_screen,width= 700, height=400,bg ='white')
+Canvas =Canvas(main_screen,width= 700, height=400,bg ='white'   )
 Canvas.pack(pady = 20)
 
 #adding images
-horse_image = PhotoImage(file="horseimage222.jpg.png")
-horse_image2 = PhotoImage(file="horseimage222.jpg.png")
-horse_image3 = PhotoImage(file="horseimage222.jpg.png")
+horse_image = PhotoImage(file="horseimage222.jpg.png", master=main_screen)
+horse_image2 = PhotoImage(file="horseimage222.jpg.png", master=main_screen)
+horse_image3 = PhotoImage(file="horseimage222.jpg.png", master=main_screen)
 #resizing images
 horse_image = horse_image.zoom(5)
 horse_image = horse_image.subsample(25)
@@ -151,8 +263,7 @@ l2.place(x=735,y=385)
 
 b1 =Button (main_screen,text='Play',height=2,width=15, bg ='white', font=('calibri',20),fg='red' ,command=start_game)
 b1.place_forget()
-b2 = Button(main_screen,text='Exit Game',height=2,width=10,bg='white',font=('calibri',10),fg='red',command= End_game)
-b2.place(x=0,y=0)
+
 b3 = Button(main_screen,text='New Round',height=2,width=10,bg='white',font=('calibri',10),fg='red',command= new_round )
 b3.place(x=700,y=0)
 #creating finish line
@@ -163,80 +274,7 @@ Canvas.create_line(coordinates, dash=(5,3) , width=9, fill='red')
 Canvas.pack()
 
 
-#creating entery box 
-def button_command():
-    global helpc
-    
-    text = entery1.get()
-    if text =='1':
-        helpc = 1
-        l5 =Label(main_screen, text = 'your horse is: horse 1',font=('calibri',20),fg='red',bg='white' )
-        l5.place(x=70,y =420)
-        
-        l1.place_forget()
-        entery1.pack_forget()
-        b3.pack_forget()
-        
-        entery2.pack()
-        b4.pack()
-        l6 =Label(main_screen, text = 'place your bet',font=('calibri',20),fg='red',bg='white' )
-        l6.place(x=330,y =380)
-    if text =='2':
-        helpc = 2
-        l5 =Label(main_screen, text = 'your horse is: horse 2',font=('calibri',20),fg='red',bg='white' )
-        l5.place(x= 70,y =420)
-        entery1.delete(0,899) 
-        l1.place_forget()   
-        entery1.pack_forget()
-        b3.pack_forget()
-        entery2.pack()
-        b4.pack()
-        l6 =Label(main_screen, text = 'place your bet',font=('calibri',20),fg='red',bg='white' )
-        l6.place(x=330,y =380)
 
-    if text =='3':
-        helpc = 3
-        l5 =Label(main_screen, text = 'your horse is: horse 3',font=('calibri',20),fg='red',bg='white' )
-        l5.place(x=70,y =420)
-        
-        l1.place_forget()  
-        entery1.pack_forget()
-        b3.pack_forget()
-        entery2.pack()
-        b4.pack()
-        l6 =Label(main_screen, text = 'place your bet',font=('calibri',20),fg='red',bg='white' )
-        l6.place(x=330,y =380)
-    return None
-
-
-
-
-
-#casino chips
-
-
-casinochips =10000
-chiptxt=Label(main_screen, text = 'chips: ' ,font=('calibri',20),fg='red',bg='white')
-chiptxt.place(x=10 ,y=500)
-lchip = Label(main_screen, text = casinochips ,font=('calibri',20),fg='red',bg='white')
-lchip.place(x=100,y=500)
-
-def button_command2():
-    textbet = entery2.get()
-    texthelp = int(textbet)
-    if texthelp>0 and texthelp<=10000:
-        lchip.place_forget()
-        chips = casinochips-texthelp
-        chip2 = Label(main_screen, text =  chips ,font=('calibri',20),fg='red',bg='white')
-        chip2.place(x=100,y=500)
-        
-      
-    b4.pack_forget()
-    entery2.pack_forget()
-    l7 =Label(main_screen, text = 'your bet is:   '+ textbet,font=('calibri',20),fg='red',bg='white' )
-    l7.place(x=330,y =380)
-    b1.place(x= 280,y=490)
-    
         
 
 
@@ -249,9 +287,7 @@ text = entery1.get()
 
 b3 = Button (main_screen,text='select', command = button_command)
 b3.pack()
-entery2 = Entry(main_screen , width=20 )
-entery2.pack_forget()
-textbet = entery2.get()
+
 
 b4 = Button (main_screen,text='bet', command = button_command2)
 b4.pack_forget()
